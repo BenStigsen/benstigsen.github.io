d6f9d38bac4b1f614c3bc6c012058c4ccee5864e
BenStigsen <benjamin.stigsen@gmail.com>
2020-12-18 01:24:52 +0100

    Initial commit

---
+++ projects/solaris/assets/js/objects.js
@@ -0,0 +1,144 @@
+/*
+  Script for data and calculations related to planets / moons / suns
+
+  Variables:
+    a:      semi-major axis
+    b:      semi-minor axis
+    e:      eccentricity
+    r:      radius
+    m:      mass
+    au:     astronomical units
+    ae:     distance from center to focal point
+    angle:  angle
+    speed:  speed in (earth ratio)
+    G:      gravitational constant
+*/
+
+function getDistance(planet)
+{
+  return Math.sqrt((
+    ((CENTER.x + planet.x) - CENTER.x) ** 2) + 
+    ((CENTER.y + planet.y) - CENTER.y) ** 2);
+}
+
+function getPeriod(planet)
+{return Math.sqrt((planet.a * planet.a * planet.a) / G) / 100;}
+
+function getPeriodDays(planet)
+{return 365.256 * (planet.period / earth.period);}
+
+function getPeriodRealSpeed(planet)
+{return (planet.period / 84000) / 365.256;}
+
+function getPerihelion(planet)
+{return planet.a / (1 + planet.e);}
+
+function getAphelion(planet)
+{return planet.a / (1 - planet.e);}
+
+function getAstronomicalUnit(planet)
+{return planet.a / earth.a;}
+
+function getEarthSpeedRatio(planet)
+{return Math.sqrt((earth.a / planet.a) ** 3);}
+
+function getSemiMajorAxis(planet)
+{return (getPerihelion(planet) + getAphelion(planet)) / 2;}
+
+function getSemiMinorAxis(planet)
+{return planet.a * Math.sqrt(1 - (planet.e * planet.e));}
+
+function getFocusPoint(planet)
+{
+  if (!(planet.b)) {return planet.a * planet.e;}
+  return Math.sqrt((planet.a * planet.a) - (planet.b * planet.b));
+}
+
+function getTriangleArea(x1, y1, x2, y2, x3, y3)
+{
+  return 0.5 * Math.abs((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)));
+}
+
+sun = {
+  m: 1.989,
+  d: 1_392_700,
+  r: 696_350 / SCALE_DIVISOR, 
+  color: "#FFDD00"
+}
+
+mercury = {
+  a: 57_900_000,
+  e: 0.205,
+  au: 0.387,
+  d: 4879,
+  color: "#333333"
+}
+
+venus = {
+  a: 108_200_000,
+  e: 0.007,
+  au: 0.723,
+  d: 12104,
+  color: "#FFDDDD"
+}
+
+earth = {
+  a:  149_600_000,  // km
+  e:  0.0167,
+  au: 1,
+  d:  12_756,       // km
+  color: "#00FF00"
+}
+
+mars = {
+  a:  227_920_000,
+  e:  0.094,
+  au: 1.52,
+  d:  6792,
+  color: "#FF7F00"
+}
+
+jupiter = {
+  a:  778_570_000,
+  e:  0.0489,
+  au: 5.20,
+  d:  142_984,
+  color: "#555555"
+}
+
+saturn = {
+  a: 1_433_500_000,
+  e: 0.057,
+  au: 9.58,
+  d: 120_536,
+  color: "#FFFF00"
+}
+
+uranus = {
+  a: 2_872_500_000,
+  e: 0.046,
+  au: 19.20,
+  d: 51_118,
+  color: "#FFFF00"
+}
+
+neptune = {
+  a: 4_495_100_000,
+  e: 0.011,
+  au: 30.05,
+  d: 49528,
+  color: "#2288FF"
+}
+
+pluto = {
+  a: 5_906_400_000,
+  e: 0.244,
+  au: 39.48,
+  d: 2370,
+  color: "#2222FF"
+}
+
+
+// Array of planets for easy iteration
+// Earth is first to apply setup changes to it
+planets = [earth, mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto];
---
+++ projects/solaris/assets/js/sketch.js
@@ -0,0 +1,171 @@
+/*
+  to-do:
+    fix focus point center (currently completely centered, which is wrong)
+*/
+
+const WIDTH             = window.innerWidth - 50;
+const HEIGHT            = window.innerHeight - 50;
+const CENTER            = new Point(WIDTH / 2, HEIGHT / 2);
+const ANGLE_INCREASE    = 0.001;
+
+const SCALE_DIVISOR     = 10000;
+const SCALE_INCREASE    = 0.1;
+const SCALE_MIN         = 0.1;
+const SCALE_MAX         = 2.5;
+
+const G                 = 6.67430;
+
+let scale               = 1.0;
+let realisticVisuals    = true; // true = realistic, false = planet 50x
+let realisticSpeed      = false;
+
+function decrementScale()
+{
+  if (!(scale <= SCALE_MIN)) 
+  {
+    scale -= SCALE_INCREASE;
+    scale = +scale.toFixed(1);
+  }
+}
+
+function incrementScale()
+{
+  if (!(scale >= SCALE_MAX)) 
+  {
+    scale += SCALE_INCREASE;
+    scale = +scale.toFixed(1);
+  }
+}
+
+function adjustDiameters()
+{
+  realisticVisuals = !realisticVisuals;
+
+  // Realistic
+  if (realisticVisuals) 
+  {
+    for (let i = 0; i < planets.length; i++)
+    {
+      planets[i].r = (planets[i].d / 2) / SCALE_DIVISOR;
+    }
+  }
+  // Planets 50x
+  else
+  {
+    for (let i = 0; i < planets.length; i++)
+    {
+      planets[i].r = ((planets[i].d / 2) / SCALE_DIVISOR) * 50;
+    }
+  }
+}
+
+function adjustSpeed()
+{
+  realisticSpeed = !realisticSpeed;
+
+  // Realistic
+  if (realisticSpeed) 
+  {
+    for (let i = 0; i < planets.length; i++)
+    {planets[i].speed = getPeriodRealSpeed(planets[i]);}
+  }
+  // Non-Realistic
+  else
+  {
+    for (let i = 0; i < planets.length; i++)
+    {planets[i].speed = getEarthSpeedRatio(planets[i]);}
+  }
+}
+
+// Function to handle setup
+function setup()
+{ 
+  // Center canvas
+  createCanvas(WIDTH, HEIGHT)
+    .position((windowWidth - WIDTH) / 2, (windowHeight - HEIGHT) / 2);
+
+  adjustDiameters();
+
+  // Create scaling buttons
+  createButton('Zoom In').mousePressed(incrementScale).position(30, 50);
+  createButton('Zoom Out').mousePressed(decrementScale).position(30, 80);
+  createButton('Realistic').mousePressed(adjustDiameters).position(30, 110);
+  createButton('Speed').mousePressed(adjustSpeed).position(30, 140);
+
+  console.log((earth.a * earth.e) / SCALE_DIVISOR);
+  // Set semi-minor axis <b> for each planet
+  for (let i = 0; i < planets.length; i++)
+  {
+    let planet    = planets[i];
+    planet.a      = planet.a / 1_000_000;
+    planet.ae     = getFocusPoint(planet);
+    planet.b      = getSemiMinorAxis(planet);
+    planet.focus  = getFocusPoint(planet);
+    planet.speed  = getEarthSpeedRatio(planet);
+    planet.period = getPeriod(planet);
+    planet.angle  = 0;
+    planet.x      = planet.a;
+    planet.y      = 0;
+  }
+}
+
+// Function to handle logic
+function update(dt)
+{
+  for (let i = 0; i < planets.length; i++)
+  {
+    let planet = planets[i];
+
+    let angle = planet.angle;
+    let a = planet.a;
+    let e = planet.e;
+    let focus = planet.focus;
+
+    angle += ((ANGLE_INCREASE * planet.speed) * dt) % 360;
+
+    let r = (a * (1 - (e * e))) / (1 + e * Math.cos(angle));
+    let x = r * cos(angle) + focus;
+    let y = r * sin(angle);
+
+    let area = getTriangleArea(-focus, 0, planet.x, planet.y, x, y);
+
+    planet.x = x;
+    planet.y = y;
+    planet.angle = angle;
+  }
+}
+
+// Function to handle drawing
+function draw()
+{
+  // Handle logic
+  update(deltaTime);
+
+  // Clear canvas and reset colors
+  clear();
+  background(220);
+  
+  fill("#000000");
+  text(scale, 30, 20);
+  text(realisticVisuals, 80, 100);
+  text(realisticSpeed, 80, 130);
+
+  // Draw the planets
+  for (let i = 0; i < planets.length; i++)
+  {
+    let planet = planets[i];
+    fill(planet.color);
+    // Planet with a minimum radius of 3 pixels
+    circle(
+      CENTER.x + (planet.ae * scale) + (planet.x * scale),
+      CENTER.y + (planet.y * scale), 
+      Math.max(5, (planet.r * scale))
+    );
+    noFill();
+    ellipse(CENTER.x + (planet.ae * scale), CENTER.y, (planet.a * 2) * scale, (planet.b * 2) * scale);
+  }
+
+  // Sun
+  fill(sun.color);
+  circle(CENTER.x, CENTER.y, sun.r * scale);
+}
