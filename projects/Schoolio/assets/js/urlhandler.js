c77b015e9815d9ce114a75530fcc7b102d38ed2d
BenStigsen <benjamin.stigsen@gmail.com>
2021-05-24 14:36:25 +0200

    Added projects/Schoolio

---
+++ projects/Schoolio/assets/js/urlhandler.js
@@ -0,0 +1,26 @@
+/// URLs should be encoded for this work properly
+
+function getUrlParameters() {
+  // Split URL parameters
+  let url = window.location.search;
+
+  if (url === undefined || url.length === 0) {return {}}
+  
+  let parameters = decodeURI(url).substring(1).split("&");
+  
+  // Get decoded keys and values from URL
+  let items = [];
+  for (let i = 0; i < parameters.length; i++) {
+    let pos = parameters[i].indexOf("=");
+    let key = parameters[i].substring(0, pos);
+    let val = parameters[i].substring(pos + 1);
+    
+    // Add map key and value
+    if (key !== -1 && val !== -1) {
+      items.push([key, val]);
+    }
+  }
+  
+  return items;
+}
+
---
+++ projects/Schoolio/assets/js/view.js
@@ -0,0 +1,47 @@
+function initView() {
+  let todolist = document.getElementById("todolist");
+
+  // Get todo items
+  let request = getUrlParameters();
+  
+  let content = new DocumentFragment();
+  for (let i = 0; i < request.length; i++) {
+    content.appendChild(addItem(request[i][0], request[i][1]));
+  }
+  
+  // Add <content> to DOM
+  todolist.appendChild(content);
+}
+
+function addItem(title, description) {
+  let fragment = new DocumentFragment();
+
+  let item               = document.createElement("div");
+  let elementTitle       = document.createElement("h1");
+  let elementDescription = document.createElement("div");
+  
+  item.classList.add("todoitem");
+  elementDescription.classList.add("description");
+  
+  elementTitle.innerText       = title;
+  elementDescription.innerHTML = description;
+  
+  item.style = "background: rgb(" + getRandomRGB(150, 225) + ")";
+  item.appendChild(elementTitle);
+  item.appendChild(elementDescription);
+  fragment.appendChild(item);
+  
+  return fragment;
+}
+
+function editItems() {
+  window.location.href = 'https://stigsen.xyz/create.html' + window.location.search;
+}
+
+function getRandomRGB(min, max) {
+  return [  
+    Math.random() * (max - min) + min,
+    Math.random() * (max - min) + min,
+    Math.random() * (max - min) + min
+  ].join(",")
+}
